f3657227c824a3c2562e8428ac0e0d3295f9d39a
Eduardo <eduardo-castanho@hotmail.com>
2017-05-30 13:12:19 -0300

    characters networks generated

--- Source/getCharacters.py
+++ Source/getCharacters.py
@@ -5,6 +5,7 @@ import os
 import re
 import string
 import sys
+import graph_tool.all
 
 def getHighestVertex (graph, vertexes):
 	degrees = []
@@ -23,15 +24,15 @@ if __name__ == '__main__':
 	blank_graph.vertex_properties['name']  = blank_graph.new_vertex_property("string")
 	for c in range(len(characters)):
 		v = blank_graph.add_vertex()
-		blank_graph.vertex_properties['name'] = char
+		blank_graph.vertex_properties['name'][c] = characters[c]
 		f.write("%d "%c + characters[c])
 		characters[c] = characters[c][:-1].split(" ")
 	f.close()
 	booksDir = "../Books/HarryPotter"
 	booksPaths = [os.path.join(booksDir, f) for f in os.listdir(booksDir)]
 	exceptions = ['Mr','Mrs','Sr','Jr']
-	for book in booksPaths[:1]:
-		g = Graph(blank_graph)
+	for book in booksPaths:
+		g = graph_tool.Graph(blank_graph)
 		with codecs.open(book, 'r', 'utf-8') as bookFile:
 			last_word = False
 			pageCharacters = []
@@ -57,7 +58,7 @@ if __name__ == '__main__':
 										indexes.append(characters.index(char))
 								if count == 0: # In case they don't match
 									if last_word not in exceptions:
-										highest = getHighestVertex(last_indexes)
+										highest = getHighestVertex(g,last_indexes)
 										pageCharacters.append(highest)
 									for char in characters:
 										if word in char:
@@ -73,14 +74,16 @@ if __name__ == '__main__':
 								last_indexes = indexes
 								indexes = []
 							else:
-								last = False
+								last_word = False
 								last_indexes = []
 							if len(indexes) == 1:
 								if indexes[0] not in pageCharacters:
 									pageCharacters.append(indexes[0])
 							elif len(indexes) > 1:
-								highest = getHighestVertex(last_indexes)
+								highest = getHighestVertex(g,last_indexes)
 								pageCharacters.append(highest)
 						else:
-							last = False
+							last_word = False
 							last_indexes = []
+		k = booksPaths.index(book)
+		g.save('../Networks/CharacterNetworks/HP_book{}.gml'.format(k+1))
